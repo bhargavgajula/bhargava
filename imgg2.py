11acd4eb728c778a915fa698184c9bee6f570aa0
bhargavgajula <113115744+bhargavgajula@users.noreply.github.com>
2023-03-02 15:01:41 +0530

    Delete imgg2.py

--- imgg2.py
+++
@@ -1,4 +0,0 @@
-import cv2 as cv
-import sys
-img=cv.imread(r"C:\Users\bhargava gajula\Pictures\Camera Roll\lord-krishna-2_082316114117.jpg",0)
-cv.imwrite(r"C:\Users\bhargava gajula\Pictures\Camera Roll\lord-krishna-2_082316114117.jpg",img)
